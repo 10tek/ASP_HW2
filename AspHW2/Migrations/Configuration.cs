3ef4ee814c8b48db9b874086c4825b92a8ae8fd9
10tekinvo@gmail.com <10tekinvo@gmail.com>
2020-07-04 23:06:51 +0600

    Добавил миграции

---
+++ AspHW2/Migrations/Configuration.cs
@@ -0,0 +1,26 @@
+﻿namespace AspHW2.Migrations
+{
+    using AspHW2.Models;
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<AspHW2.Data.StudentDataContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+            ContextKey = "AspHW2.Data.StudentDataContext";
+        }
+
+        protected override void Seed(AspHW2.Data.StudentDataContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method
+            //  to avoid creating duplicate seed data.
+            
+        }
+    }
+}
